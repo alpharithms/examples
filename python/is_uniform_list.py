d8c09b989dad5ca2ff28eabee7c8d117b704d754
Zack West <43519801+alphazwest@users.noreply.github.com>
2021-11-26 17:57:46 -0500

    Create is_uniform_list.py

---
+++ python/is_uniform_list.py
@@ -0,0 +1,7 @@
+def is_uniform_list(items: list) -> bool:
+    """
+    Given a list, determines if elements are of equal length.
+    Returns:
+        True if elements are all of equal length else False
+    """
+    return not any(len(x) != len(y) for x in items for y in items if x != y)
